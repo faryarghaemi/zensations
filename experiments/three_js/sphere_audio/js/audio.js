bca24f91a62f4857f68c7ded805f4989c097f1c1
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-27 17:45:10 +1100

    refactored code

--- experiments/three_js/sphere_audio/js/audio.js
+++ experiments/three_js/sphere_audio/js/audio.js
@@ -19,64 +19,6 @@ $(document).ready(function() {
       alert('Web Audio API is not supported in this browser');
   };
 
-  // ------------------------- Three.js Stuff -------------------------
-
-  var scene = new THREE.Scene();
-  var width = window.innerWidth * 0.03;
-  var height = window.innerHeight * 0.05;
-  var camera = new THREE.OrthographicCamera( 0, width, height / 2, height / - 2, 1, 10 );
-  scene.add( camera );
-
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
-
-  var geometry = new THREE.SphereGeometry( 1, 24, 24 );
-  var material = new THREE.MeshBasicMaterial( { color: 0xFF3456, wireframe: true } );
-
-  for ( var i = 8; i > 0; i -- ) {
-    var sphere = new THREE.Mesh( geometry, material );
-
-    var myPosition = (i + 1) * 3
-    sphere.position.x += myPosition;
-
-    var myScale = 1.1 * (i + 1);
-    sphere.scale.y *= myScale;
-
-    scene.add( sphere );
-  }
-
-  camera.position.z = 10;
-
-  var render = function () {
-    requestAnimationFrame( render );
-
-    if (music_playing) {
-      console.log(frequencyAmplitudeArray);
-    };
-
-    // index of frequencies array
-    var j = 0;
-
-    scene.traverse (function (object)
-    {
-        if (object instanceof THREE.Mesh)
-        {
-          object.rotation.y += 0.005;
-          if (music_playing) {
-            object.scale.y = (j + 1) * (1 + frequencyAmplitudeArray[j]/512)
-            j++
-          };
-        };
-    });
-
-    renderer.render(scene, camera);
-  };
-
-  render();
-
-  // ------------------------- End three.js -------------------------
-
   // Creating an Audio object.
   var audio0 = new Audio();
   audio0.src = 'assets/feeling_good.mp3';
@@ -113,7 +55,8 @@ $(document).ready(function() {
   // Uint8Array = Unsigned Integer 8bit byte Array
   // Values between 0-255 will be pushed into this array
   // Which represent -1 to +1 (in audio terms)
-  var frequencyAmplitudeArray = new Uint8Array(analyserNode.frequencyBinCount);
+  // Note that this is a global
+  frequencyAmplitudeArray = new Uint8Array(analyserNode.frequencyBinCount);
 
   // Copies the current time-domain (waveform) data into the passed frequencyAmplitudeArray.
   var getFrequencies = function() {
@@ -130,6 +73,8 @@ $(document).ready(function() {
       // Get the Time Domain data for this sample
       analyserNode.getByteFrequencyData(frequencyAmplitudeArray);
     }
+    // Render in three.js
+    render();
   });
 
   // Stop sound & visualise
