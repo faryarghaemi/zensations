4b2ab86e5c065c335fdc8eb06dd856c6284ec362
Faryar Ghaemi <faryarg88@gmail.com>
2015-02-27 18:02:42 +1100

    moving object with leap

--- experiments/leap_and_three/js/leapandthree.js
+++ experiments/leap_and_three/js/leapandthree.js
@@ -1,14 +1,45 @@
+var cube;
+
+
 
 
 $(document).ready(function() {
 
-var position = function(x,y,z) { 
-  debugger;
-  cube.position.x += x; 
-  cube.position.y += y;
-  cube.position.z += z; 
+var canvasElement = $("canvas")
 
-  console.log(x, y, z); 
+
+var controller = new Leap.Controller();
+controller.on("frame", function(frame){
+    if(frame.pointables.length > 0)
+    {
+        canvasElement.width = canvasElement.width; //clear
+        
+        //Get a pointable and normalize the tip position
+        var pointable = frame.pointables[0];
+        var interactionBox = frame.interactionBox;
+        var normalizedPosition = interactionBox.normalizePoint(pointable.tipPosition, true);
+        
+        // Convert the normalized coordinates to span the canvas
+        var canvasX = canvasElement.width * normalizedPosition[0];
+        var canvasY = canvasElement.height * (1 - normalizedPosition[1]);
+        //we can ignore z for a 2D context
+        
+        // displayArea.strokeText("(" + canvasX.toFixed(1) + ", " + canvasY.toFixed(1) + ")", canvasX, canvasY);
+    }
+});
+controller.connect();
+var position = function(x,y,z) { 
+  // debugger;
+  // console.log("X: " + x + " Y: " + y + " Z: " + z);
+
+  cube.position.x = x + window.innerWidth; 
+
+  cube.position.y = ( y - window.innerHeight / 2)  ;
+  // cube.position.y = parseFloat(y  - window.innerHeight) + 500;
+
+  cube.position.z = parseFloat(z);   
+// debugger;
+  // console.log(x, y, z); 
   
 } 
 
@@ -16,6 +47,8 @@ var position = function(x,y,z) {
   Leap.loop({
       hand: function(hand) {
         var screenPosition = hand.screenPosition(hand.palmPosition);
+        // debugger;
+        console.log(hand.palmPosition);
         var outputContent = "x: " + (screenPosition[0].toPrecision(4)) + 'px' +
           "        <br/>y: " + (screenPosition[1].toPrecision(4)) + 'px' +
           "        <br/>z: " + (screenPosition[2].toPrecision(4)) + 'px';
@@ -26,7 +59,12 @@ var position = function(x,y,z) {
         );
         output.html(outputContent);
 
-        position(screenPosition[0].toPrecision(4), screenPosition[1].toPrecision(4), screenPosition[2].toPrecision(4)); 
+        var x = (hand.palmPosition[0].toPrecision(4));
+        var y = (hand.palmPosition[1].toPrecision(4));
+        var z = hand.palmPosition[2].toPrecision(4);
+
+
+        position(x, y, z); 
 
       }
 
@@ -45,11 +83,11 @@ var position = function(x,y,z) {
   document.body.appendChild(renderer.domElement);
 
 
-  var geometry = new THREE.BoxGeometry(10, 10, 10);
+  var geometry = new THREE.BoxGeometry(100, 100, 100);
   var material = new THREE.MeshBasicMaterial({
     color: 0xff00ff
   });
-  var cube = new THREE.Mesh(geometry, material);
+  cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
 
   camera.position.x = -1; 
