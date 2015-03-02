76660f4586200b9cdd76abd970cc0a25d23ebf13
liamdarmody <liamdarmody@gmail.com>
2015-03-02 15:52:51 +1100

    Added Users Controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def edit 
+    @user = User.find params[:id]
+    
+    unless @user == @current_user
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+    @user.update user_params
+    redirect_to @user
+  end
+
+  def signup_login
+    session[:destination] ||= request.env["HTTP_REFERER"]
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
