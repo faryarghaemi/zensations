8dd6151d261687171e8b37335c94e2c893c611cb
Mario Martinez <zenitram.oiram@gmail.com>
2015-03-03 14:21:02 +1100

    page layout bass.css

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
   $('a').on('click',function() {
-    if($('#slide-menu').css('left')=='0px'){
-        $('#slide-menu').animate({left: '-300px'}, 500);
-        $('.audio-controls').fadeOut(500);        
+    if($('#slide-menu').css('bottom')=='0px'){
+        $('#slide-menu').animate({bottom: '-300px'}, 500);      
     }else{
-        $('#slide-menu').animate({left:0}, 500); 
-        $('.audio-controls').fadeIn(500);        
+        $('#slide-menu').animate({bottom:0}, 500);        
     }
   });
 });
