8dc3ebb23f23dd5a48702125cd5aa48abe4ce7f0
liamdarmody <liamdarmody@gmail.com>
2015-03-04 12:05:08 +1100

    Moved SpaceSlug visualisation into Javascript file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
   def nav_menu_right
 
     if @current_user.present?
-
       links = "<li>#{ link_to('Sign Out ' + @current_user.name, login_path, :method => :delete) }</li>"
     else
       links = "<li>#{ link_to('Sign Up', new_user_path) }</li><li>#{ link_to('Log In', login_path) }</li>"
